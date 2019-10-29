fb968016edef1f7e8ce2e59bfbc37d491b15f96b
PROYASH PODDER <proyash.podder@gmail.com>
2019-10-29 12:56:22 -0400

    Add scenarios

---
+++ scenarios/v2vscene.cpp
@@ -0,0 +1,239 @@
+#include "ns3/lte-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/config-store.h"
+#include <cfloat>
+#include <sstream>
+#include <cmath>
+#include "ns3/ndnSIM-module.h"
+
+using namespace ns3;
+
+
+NS_LOG_COMPONENT_DEFINE ("LteSlOutOfCovrg");
+
+int main (int argc, char *argv[])
+{
+  Time simTime = Seconds (6);
+  bool enableNsLogs = false;
+  bool useIPv6 = false;
+  //double distance=atoi(argv[1]);
+
+  CommandLine cmd;
+  cmd.AddValue ("simTime", "Total duration of the simulation", simTime);
+  cmd.AddValue ("enableNsLogs", "Enable ns-3 logging (debug builds)", enableNsLogs);
+ // cmd.AddValue("distance", "Distance apart to place nodes (in meters).",distance);
+
+  cmd.Parse (argc, argv);
+
+  //Configure the UE for UE_SELECTED scenario
+  Config::SetDefault ("ns3::LteUeMac::SlGrantMcs", UintegerValue (16));
+  Config::SetDefault ("ns3::LteUeMac::SlGrantSize", UintegerValue (5)); //The number of RBs allocated per UE for Sidelink
+  Config::SetDefault ("ns3::LteUeMac::Ktrp", UintegerValue (1));
+  Config::SetDefault ("ns3::LteUeMac::UseSetTrp", BooleanValue (true)); //use default Trp index of 0
+
+  //Set the frequency
+  uint32_t ulEarfcn = 18100;
+  uint16_t ulBandwidth = 75;
+
+  // Set error models
+  Config::SetDefault ("ns3::LteSpectrumPhy::SlCtrlErrorModelEnabled", BooleanValue (true));
+  Config::SetDefault ("ns3::LteSpectrumPhy::SlDataErrorModelEnabled", BooleanValue (true));
+  Config::SetDefault ("ns3::LteSpectrumPhy::DropRbOnCollisionEnabled", BooleanValue (false));
+
+  ConfigStore inputConfig;
+  inputConfig.ConfigureDefaults ();
+  // parse again so we can override input file default values via command line
+  cmd.Parse (argc, argv);
+
+  if (enableNsLogs)
+    {
+      LogLevel logLevel = (LogLevel)(LOG_PREFIX_FUNC | LOG_PREFIX_TIME | LOG_PREFIX_NODE | LOG_LEVEL_ALL);
+
+      LogComponentEnable ("LteUeRrc", logLevel);
+      LogComponentEnable ("LteUeMac", logLevel);
+      LogComponentEnable ("LteSpectrumPhy", logLevel);
+      LogComponentEnable ("LteUePhy", logLevel);
+    }
+
+  //Set the UEs power in dBm
+  Config::SetDefault ("ns3::LteUePhy::TxPower", DoubleValue (23.0));
+
+  //Sidelink bearers activation time
+  Time slBearersActivationTime = Seconds (2.0);
+
+  //Create the helpers
+  Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+
+  //Create and set the EPC helper
+  Ptr<PointToPointEpcHelper>  epcHelper = CreateObject<PointToPointEpcHelper> ();
+  lteHelper->SetEpcHelper (epcHelper);
+
+  ////Create Sidelink helper and set lteHelper
+  Ptr<LteSidelinkHelper> proseHelper = CreateObject<LteSidelinkHelper> ();
+  proseHelper->SetLteHelper (lteHelper);
+
+  //Enable Sidelink
+  lteHelper->SetAttribute ("UseSidelink", BooleanValue (true));
+
+  //Set pathloss model
+  lteHelper->SetAttribute ("PathlossModel", StringValue ("ns3::Cost231PropagationLossModel"));
+  // channel model initialization
+  lteHelper->Initialize ();
+
+  // Since we are not installing eNB, we need to set the frequency attribute of pathloss model here
+  double ulFreq = LteSpectrumValueHelper::GetCarrierFrequency (ulEarfcn);
+  NS_LOG_LOGIC ("UL freq: " << ulFreq);
+  Ptr<Object> uplinkPathlossModel = lteHelper->GetUplinkPathlossModel ();
+  Ptr<PropagationLossModel> lossModel = uplinkPathlossModel->GetObject<PropagationLossModel> ();
+  NS_ABORT_MSG_IF (lossModel == NULL, "No PathLossModel");
+  bool ulFreqOk = uplinkPathlossModel->SetAttributeFailSafe ("Frequency", DoubleValue (ulFreq));
+  if (!ulFreqOk)
+    {
+      NS_LOG_WARN ("UL propagation model does not have a Frequency attribute");
+    }
+
+  NS_LOG_INFO ("Deploying UE's...");
+
+  //Create nodes (UEs)
+  NodeContainer ueNodes;
+  ueNodes.Create (9);
+  NS_LOG_INFO ("UE 1 node id = [" << ueNodes.Get (0)->GetId () << "]");
+  NS_LOG_INFO ("UE 2 node id = [" << ueNodes.Get (1)->GetId () << "]");
+  NS_LOG_INFO ("UE 3 node id = [" << ueNodes.Get (2)->GetId () << "]");
+
+  MobilityHelper mobility;
+  Ptr<ListPositionAllocator> initialAlloc = CreateObject<ListPositionAllocator> ();
+  //this node will generate the interest
+  initialAlloc->Add(Vector(0.0,0.0,0.0));
+  //these nodes will receive the interest in 1st hop
+  initialAlloc->Add(Vector(50.0,-100.0,0.0));
+  initialAlloc->Add(Vector(100.0,0.0,0.0));
+  initialAlloc->Add(Vector(300.0,150.0,0.0));
+  //these nodes will receive the interest in first hop and drop it as they are in opposite direction from the destination
+  initialAlloc->Add(Vector(-50.0,0.0,0.0));
+  initialAlloc->Add(Vector(-50,150.0,0.0));
+  initialAlloc->Add(Vector(-25.0,-100.0,0.0));
+  //this is the producer node
+  initialAlloc->Add(Vector(650.0,200.0,0.0));
+  //this node will drop the interest since it is far away from the destination
+  initialAlloc->Add(Vector(850.0,250.0,0.0));
+
+  mobility.SetPositionAllocator(initialAlloc);
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install(ueNodes.Get (0));
+  mobility.Install(ueNodes.Get (1));
+  mobility.Install(ueNodes.Get (2));
+  mobility.Install(ueNodes.Get (3));
+  mobility.Install(ueNodes.Get (4));
+  mobility.Install(ueNodes.Get (5));
+  mobility.Install(ueNodes.Get (6));
+  mobility.Install(ueNodes.Get (7));
+  mobility.Install(ueNodes.Get (8));
+
+
+
+
+
+
+
+
+
+  //Install LTE UE devices to the nodes
+  NetDeviceContainer ueDevs = lteHelper->InstallUeDevice (ueNodes);
+
+  //Sidelink pre-configuration for the UEs
+  Ptr<LteSlUeRrc> ueSidelinkConfiguration = CreateObject<LteSlUeRrc> ();
+  ueSidelinkConfiguration->SetSlEnabled (true);
+
+  LteRrcSap::SlPreconfiguration preconfiguration;
+
+  preconfiguration.preconfigGeneral.carrierFreq = ulEarfcn;
+  preconfiguration.preconfigGeneral.slBandwidth = ulBandwidth;
+  preconfiguration.preconfigComm.nbPools = 1;
+
+  LteSlPreconfigPoolFactory pfactory;
+
+  //Control
+  pfactory.SetControlPeriod ("sf40");
+  pfactory.SetControlBitmap (0x00000000FF); //8 subframes for PSCCH
+  pfactory.SetControlOffset (0);
+  pfactory.SetControlPrbNum (22);
+  pfactory.SetControlPrbStart (0);
+  pfactory.SetControlPrbEnd (49);
+
+  //Data
+  pfactory.SetDataBitmap (0xFFFFFFFFFF);
+  pfactory.SetDataOffset (8); //After 8 subframes of PSCCH
+  pfactory.SetDataPrbNum (25);
+  pfactory.SetDataPrbStart (0);
+  pfactory.SetDataPrbEnd (49);
+
+  preconfiguration.preconfigComm.pools[0] = pfactory.CreatePool ();
+
+  ueSidelinkConfiguration->SetSlPreconfiguration (preconfiguration);
+  lteHelper->InstallSidelinkConfiguration (ueDevs, ueSidelinkConfiguration);
+
+  InternetStackHelper internet;
+  internet.Install (ueNodes);
+  uint32_t groupL2Address = 255;
+  Ipv4Address groupAddress4 ("225.63.63.1");     //use multicast address as destination
+
+  Ipv4InterfaceContainer ueIpIface;
+  ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueDevs));
+
+  // set the default gateway for the UE
+  Ipv4StaticRoutingHelper ipv4RoutingHelper;
+  for (uint32_t u = 0; u < ueNodes.GetN (); ++u) {
+    Ptr<Node> ueNode = ueNodes.Get(u);
+    // Set the default gateway for the UE
+    Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting(ueNode->GetObject<Ipv4>());
+    ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress(), 1);
+  }
+
+  Address remoteAddress = InetSocketAddress (groupAddress4, 8000);
+  Address localAddress = InetSocketAddress (Ipv4Address::GetAny (), 8000);
+  Ptr<LteSlTft> tft = Create<LteSlTft> (LteSlTft::BIDIRECTIONAL, groupAddress4, groupL2Address);
+
+  ///*** Configure applications ***///
+
+  //Set Sidelink bearers
+  proseHelper->ActivateSidelinkBearer (slBearersActivationTime, ueDevs, tft);
+
+  ///*** End of application configuration ***///
+  ::ns3::ndn::StackHelper helper;
+  helper.SetDefaultRoutes(true);
+  helper.InstallAll();
+
+  //* Choosing forwarding strategy *//
+  ns3::ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/directed-geocast");
+
+ //Will add cost231Propagationloss model loss here for and packet loss
+
+  // Consumer
+  ::ns3::ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
+  // Consumer will request /prefix/0, /prefix/1, ...
+  //consumerHelper.SetPrefix("/v2safety/8thStreet/parking");
+  consumerHelper.SetPrefix("/v2safety/8thStreet/0,0,0/700,0,0/100");
+  consumerHelper.SetAttribute("Frequency", StringValue("1")); // 10 interests a second
+  consumerHelper.Install(ueNodes.Get(0));                        // first node
+
+  // Producer
+  ::ns3::ndn::AppHelper producerHelper("ns3::ndn::Producer");
+  // Producer will reply to all requests starting with /prefix
+  producerHelper.SetPrefix("/v2safety/8thStreet");
+  producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
+  producerHelper.Install(ueNodes.Get(7));
+
+  //ns3::ndn::L3RateTracer::InstallAll("trace.txt", Seconds(1));
+  Simulator::Stop (Seconds(4));
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+
+}
