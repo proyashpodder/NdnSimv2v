c0cf5edc5417c8dd526b039c3c0e5f135575d322
Alexander Afanasyev <aa@cs.fiu.edu>
2020-08-19 23:51:16 -0400

    Add communication overhead graphs

---
+++ graphs/communication.R
@@ -0,0 +1,111 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(plyr))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(reshape2))
+
+# install.packages('doBy')
+suppressPackageStartupMessages(library(doBy))
+
+source("graphs/graph-style.R")
+source("graphs/helpers.R")
+
+
+ranges = data.frame(min=c(1.5, 1), max=c(3, 3))
+## minList = c(0.5, 1, 1.5)
+## rangeList = c(1.5, 2, 2.5, 3, 3.5)
+## for (m in minList) {
+##     for (r in rangeList) {
+##         ranges = ranges %>% add_row(min=m, max=m+r)
+##     }
+## }
+
+levels = c(
+    "0.5-2 m (range 1.5 m)",
+    "1-2.5 m (range 1.5 m)",
+    "1.5-3 m (range 1.5 m)",
+
+    "0.5-2.5 m (range 2 m)",
+    "1-3 m (range 2 m)",
+    "1.5-3.5 m (range 2 m)",
+
+    "0.5-3 m (range 2.5 m)",
+    "1-3.5 m (range 2.5 m)",
+    "1.5-4 m (range 2.5 m)",
+
+    "0.5-3.5 m (range 3 m)",
+    "1-4 m (range 3 m)",
+    "1.5-4.5 m (range 3 m)",
+
+    "0.5-4 m (range 3.5 m)",
+    "1-4.5 m (range 3.5 m)",
+    "1.5-5 m (range 3.5 m)"
+)
+
+
+data = c()
+for (r in 1:nrow(ranges)) {
+    for (run in 1:10) {
+        tryCatch({
+            mi = ranges[r, "min"]
+            ma = ranges[r, "max"]
+
+            f = file(paste(sep='', "results/3-rates-rates-run-", run ,"-min-", format(mi, nsmall=6),"-max-", format(ma, nsmall=6),".csv"))
+            d = read.table(f, header=TRUE)
+            d$Run = run
+            d$Adjustment = factor(paste(sep='', mi, '-', ma, ' m (range ', ma-mi, ' m)'), levels=levels, ordered=TRUE)
+            if (length(data) == 0) {
+                data = d
+            } else {
+                data = rbind(d, data)
+            }
+        }, warning = function(w) {
+            print(w)
+        }, error = function(e) {
+            print(e)
+        }, finally = {
+        })
+    }
+}
+
+data = subset(data, FaceDescr=="lte://" & (Type == "OutInterests" | Type == "InInterests" | Type == "OutData" | Type == "InData"))
+
+data$Type = factor(data$Type)
+data$FaceDescr = factor(data$FaceDescr)
+data$Node = factor(data$Node)
+data$FaceId = factor(data$FaceId)
+data$Kilobits = data$Kilobytes * 8
+
+# combine stats from all faces
+data.combined = summaryBy(. ~ Run + Adjustment + Time + Type, data=data, FUN=sum)
+
+data.s = summarySE(data.combined, measurevar=c("Packets.sum"), groupvars=c("Adjustment", "Time", "Type"))
+
+data.x = subset(data.s, Type == "OutInterests" | Type == "OutData")
+
+g <- ggplot(data.x, aes(x=Time, y=Packets.sum, color=Type)) +
+    geom_point(size=1) +
+    geom_line(size=0.2) +
+    geom_errorbar(aes(ymin=Packets.sum-se, ymax=Packets.sum+se), width=4, size=0.2, position=position_dodge(0.2)) +
+    ylab("Number of (interest/data) packets per second") +
+    theme_custom() +
+    facet_wrap(~ Adjustment)
+
+ggsave("graphs/pdfs/rates-1-packets.pdf", plot=g, width=9, height=5, device=cairo_pdf)
+
+
+data.s2 = summarySE(data.combined, measurevar=c("Kilobits.sum"), groupvars=c("Adjustment", "Time", "Type"))
+
+data.x2 = subset(data.s2, Type == "OutInterests" | Type == "OutData")
+
+g <- ggplot(data.x2, aes(x=Time, y=Kilobits.sum, color=Type)) +
+    geom_point(size=1) +
+    geom_line(size=0.2) +
+    geom_errorbar(aes(ymin=Kilobits.sum-se, ymax=Kilobits.sum+se), width=4, size=0.2, position=position_dodge(0.2)) +
+    ylab("Number of (interest/data) packets per second") +
+    theme_custom() +
+    facet_wrap(~ Adjustment)
+
+ggsave("graphs/pdfs/rates-1-kilobits.pdf", plot=g, width=9, height=5, device=cairo_pdf)
