c92de8aa32b612d970c618bdf843ec39cfe53100
Alexander Afanasyev <aa@cs.fiu.edu>
2021-04-21 19:17:31 -0400

    Fix data in the graph
    Now it should be correct (matched old)

--- graphs/comparison-multi-hops.R
+++ graphs/comparison-multi-hops.R
@@ -56,12 +56,12 @@ for (den in Density){
     for (ped in pedCount){
         for (r in 1:10) {
             filename = paste(sep='', FOLDER,'/nowTime-',r,'-0.0001-0.5-',den,'-',ped,'-ped-12-poi-6-pro-300-consumerdistance.csv')
-            filenameProcessed = paste(sep='', filename, '-processed.csv')
-            if (file.exists(filenameProcessed)) {
-                f = file(filenameProcessed)
-                d = read.table(f, header=TRUE)
-            }
-            else {
+            ## filenameProcessed = paste(sep='', filename, '-processed.csv')
+            ## if (file.exists(filenameProcessed)) {
+            ##     f = file(filenameProcessed)
+            ##     d = read.table(f, header=TRUE)
+            ## }
+            ## else {
                 f = file(filename)
                 d = read.table(f,header=TRUE)
                 d = subset(d, FaceDescr=="lte://" & (Type == "OutInterests" | Type == "OutData"))
@@ -73,7 +73,7 @@ for (den in Density){
                 d <-  d %>%
                     select(Time,Type,PacketRaw,Distance) %>%
                     group_by(Time = cut(Time, breaks = seq(0.0,60.0,10)),
-                             Distance = cut(Distance, breaks = seq(0, 501, 60)),
+                             Distance = cut(Distance, breaks = c(0,60,120,240,360,480)),
                              PacketType = Type) %>%
                     summarize(PacketRaw = sum(PacketRaw))
                 
@@ -86,8 +86,8 @@ for (den in Density){
                 else
                     d$Type <- factor("High Density")
 
-                write.table(d, filenameProcessed)
-            }
+            ##     write.table(d, filenameProcessed)
+            ## }
 
             if(length(data) == 0){
                 data =  d
@@ -112,11 +112,23 @@ dd <- ru %>% group_by(Distance,PacketType,Type,Count) %>%
 ru2 <- data %>% group_by(Distance,Type,Count,Run) %>% summarise(PacketRaw = sum(PacketRaw))
 
 # dd2 data without packet types
-dd2 <- ru %>% group_by(Distance,Type,Count) %>%
+dd2 <- ru2 %>% group_by(Distance,Type,Count) %>%
     summarise(Mean=mean(PacketRaw),StdDev=sd(PacketRaw),NPoints=n()) %>%
     mutate(Min=Mean - qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints),
            Max=Mean + qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints)) %>%
     ungroup()
+
+ru3 <- data %>% group_by(Type,Count,Run) %>% summarise(PacketRaw = sum(PacketRaw))
+
+# dd3 data without packet types and distances
+dd3 <- ru3 %>% group_by(Type,Count) %>%
+    summarise(Mean=mean(PacketRaw),StdDev=sd(PacketRaw),NPoints=n()) %>%
+    mutate(Min=Mean - qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints),
+           Max=Mean + qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints)) %>%
+    ungroup()
+dd3$Type = factor(dd3$Type, levels=c("Low Density", "Medium Density", "High Density", "Baseline"), ordered=TRUE)
+dd3$Count <- factor(dd3$Count, levels=pedCount, ordered=FALSE)
+
 
 
 b <- read.csv(file=paste(sep='', FOLDER, "/baseline-ped-40.csv"))
@@ -134,19 +146,18 @@ b$Type = "Baseline"
 ## g  <- ggplot(data) +
 ##     geom_point(aes(x=Time, y=PacketRaw, group=Type, colour=Distance, shape=PacketType))
 
-
-bb$Count = factor(bb$Count, levels=c(40,80,160,320,640), ordered=TRUE)
-bb$Distance = factor("(0,120]")
+b$Count = factor(b$Count, levels=c(40,80,160,320,640), ordered=TRUE)
+b$Distance = factor("(0,60]")
 ## bb$PacketType = factor("IP")
 
-bdd <- bb %>% group_by(Distance, Type, Count) %>%
+bdd <- b %>% group_by(Distance, Type, Count) %>%
     summarise(Mean=sum(PacketRaw),StdDev=0,NPoints=1) %>%
     mutate(Min=Mean - qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints),
            Max=Mean + qt(1 - (0.05 / 2), NPoints - 1) * StdDev/sqrt(NPoints)) %>%
     ungroup()
 
 bd = rbind(bdd,dd2)
-bd$Type = factor(bd$Type, levels=c("Low Density","Medium Density","High Density","Baseline"), ordered=TRUE)
+bd$Type = factor(bd$Type, levels=c("Low Density", "Medium Density", "High Density", "Baseline"), ordered=TRUE)
 
                                         #g <- ggplot(bd,aes(x=Time,y=PacketRaw, color=Type)) + geom_line(size=0.6)+theme_custom() +facet_wrap(~Count) +ggtitle("Comparison among baseline, singlehop and multihop total traffic for various pedestrain count") + xlab("Time(s)") + ylab("Number of Total Packets (Mean of 20 runs)")
 
@@ -165,14 +176,14 @@ for (row in 1:nrow(bd1)) {
     bd2[row,]$Mean = adjusted
 }
 
+ddd3 = rbind(dd3, subset(bdd, select=-Distance))
 
 g <- ggplot(data=bd2, aes(x=Count,
                          y=Mean,
                          group=Type,
-                         linetype=Distance,
                          fill=Type)) +
-    geom_bar(position="dodge", stat="identity", colour="black", alpha=0.1) +
-    geom_errorbar(aes(ymin=Min, ymax=Max), size=I(0.3), width=I(0.4), position=position_dodge(width=1))+
+    geom_bar(aes(linetype=Distance), position="dodge", stat="identity", colour="black", alpha=0.1) +
+    geom_errorbar(data=ddd3, aes(ymin=Min, ymax=Max), size=I(0.3), width=I(0.2), position=position_dodge(width=0.9))+
     theme_custom()+
     ggtitle("Comparison among baseline, Low, Medium and High Density vehicle in single hop scenario for various pedestrain count") + xlab("Number of Pedestrians") +
     ylab("Number of Total Packets (Mean of 10 runs)")
